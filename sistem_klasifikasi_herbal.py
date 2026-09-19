403dd523a23b71ed553c385b6c7ae6a04a1668fa
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2026-09-19 17:15:16 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -59,7 +59,7 @@ CONFIG = {
     "TARGET_BRIGHTNESS": 110,
     "CLAHE_CLIP": 4.0,
     "CLAHE_TILE": (4, 4),
-    "VEIN_STRENGTH": 0.4
+    "VEIN_STRENGTH": 1.2
 }
 
 # =========================================================
